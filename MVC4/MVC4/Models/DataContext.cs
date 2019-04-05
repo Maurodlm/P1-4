729ed98c772e71bff8fd6f83fad3efa1ca7bfd84
MAURO ALEJANDRO HERNANDEZ DE LAS MUÑECAS <mauroahdlm@gmail.com>
2019-04-05 13:57:58 -0400

    commit con modelo mvc

---
+++ MVC4/MVC4/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC4.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<MVC4.Models.Products> Products { get; set; }
+    }
+}
---
+++ MVC4/MVC4/Models/Products.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC4.Models
+{
+    public enum Unitype
+    {
+        Botella,
+        Metro,
+        Litro,
+        Caja
+    }
+    public enum StatusType
+    {
+        Activo, 
+        Inactivo
+    }
+
+    public class Products
+    {
+        [Key]
+        public int ProductsId { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        [Required]
+        public Unitype Unit { get; set; }
+        public StatusType Status { get; set; }
+    }
+}
